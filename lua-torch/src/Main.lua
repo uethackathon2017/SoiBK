666cf223670ca6d0ad29c5043f03cc53dd6f4e16
Nguyen Minh Phuong <phuongnm.bkhn@gmail.com>
2017-03-11 03:45:08 +0700

    continue build model and debug follow paper

--- lua-torch/src/Main.lua
+++ lua-torch/src/Main.lua
@@ -3,10 +3,80 @@ require("Package")
 require("Model")
 
 opt = ParamsParser()
-model = RnnSemanticParser.model.Seq2Seq()
+torch.manualSeed(1)
+ opt = {}
+ opt.rnn_size = 10
+
+linear = nn.Linear(2*opt.rnn_size, opt.rnn_size)
+-- linear.weight = torch.rand(2*opt.rnn_size, opt.rnn_size):t()
+-- print (linear(torch.Tensor(2*opt.rnn_size):fill(7)))
+-- model = RnnSemanticParser.model.Seq2Seq()
 
 x1  = torch.Tensor({{0,2,5}, {3,4,5}}):t()
 x2  = torch.Tensor({{2,5,6,7,0}, {2,3,4,5,0}}):t()
+y1  = torch.Tensor({{5,6,7,8,0}, {3,4,5,8,0}}):t()
 
-y = model:forward(x1, x2)
-print (y)
+-- y = model:forward(x1, x2, y1)
+-- y = model:backward(x1, x2, y1)
+x1  = torch.rand(3,5,10)--:fill(1)
+x2  = torch.rand(3,7,10) -- :fill(2)
+x2[1]:fill(1)
+x2[2]:fill(2)
+x2[3]:fill(3)
+
+ enc_s_top = x1 
+ dec_s_top = torch.rand(3,10)--(nn.SplitTable(2)(x2))[1]
+ dec_s_top[1]:fill(1)
+ dec_s_top[2]:fill(2)
+ dec_s_top[3]:fill(3)
+--  print (enc_s_top, dec_s_top)
+
+ -- (batch*length*H) * (batch*H*1) = (batch*length*1)
+  local dot = nn.MM()({enc_s_top, nn.View(opt.rnn_size ,1):setNumInputDims(1)(dec_s_top)})
+  local attention = nn.SoftMax()(nn.Sum(3)(dot))
+--   print (attention)
+  -- (batch*length*H)^T * (batch*length*1) = (batch*H*1)
+  local enc_attention = nn.MM(true, false)({enc_s_top, nn.View(-1, 1):setNumInputDims(1)(attention)})
+  print ("hid")
+  print(nn.Sum(3)(enc_attention))
+  local hid = nn.Tanh()(linear(nn.JoinTable(2)({nn.Sum(3)(enc_attention), dec_s_top})))
+  print ("hid")
+  print (hid)
+  -- local h2y_in = hid
+
+    local model = nn.Sequential()
+
+    local encodeAttention = nn.Sequential()
+                              :add(nn.MM(false, true))
+                              :add(nn.SplitTable(3))
+                              :add(nn.Sequencer(nn.SoftMax()))
+                              :add(nn.JoinTable(1))
+                              :add(nn.View(7,3,5))  -- seqDec x batch x seqEnc 
+                              :add(nn.Transpose({1,2}, {2,3}))  -- seqDec x batch x seqEnc 
+
+    local attentionLayer = nn.ConcatTable()
+                                :add(
+                                    nn.Sequential()
+                                        :add( nn.ConcatTable()
+                                                  :add(nn.SelectTable(1))
+                                                  :add(encodeAttention)
+                                        )
+                                        :add(nn.MM(true, false))
+                                )
+                                :add( nn.Sequential()
+                                                :add(nn.SelectTable(2))
+                                                :add(nn.Transpose({2,3}))
+                                )
+    model   :add(attentionLayer)
+            -- :add(nn.JoinTable(2))
+            -- :add(nn.SplitTable(3))
+            -- :add(nn.Sequencer(linear))
+            -- :add(nn.Sequencer(nn.Tanh()))
+    
+    out =  (model:forward({x1, x2}))
+    print (out)
+    print (out[1])
+    -- print (out[1][2])
+
+    -- print (out[2][1])
+    -- print (out[2][2])
--- lua-torch/src/Model.lua
+++ lua-torch/src/Model.lua
@@ -9,10 +9,12 @@ function Seq2Seq:__init()
 
     local lengDict = 100
     local lengLabel = 40
+    local batchSize = 3
     local lengWordVector = 5
 
     --
     -- init encode layer 
+    -- batch x seqEncodeSize[word-encode id] => batch x SeqLengEncode x HiddenSize [double value]
     local lookupTableE  = nn.LookupTableMaskZero(lengDict, lengWordVector)
     self.lstmEncoder    = nn.FastLSTM(lengWordVector, lengWordVector)
     local switchLayer   = nn.ConcatTable()              -- 1 input => n output
@@ -25,6 +27,7 @@ function Seq2Seq:__init()
 
     --
     -- init decode layer 
+    -- batch x seqEncodeSize[word-decode id] => batch x SeqLengDecode x HiddenSize [double value]
     local   lookupTableD  = nn.LookupTableMaskZero(lengDict, lengWordVector)
     self.lstmDecoder      = nn.FastLSTM(lengWordVector, lengWordVector)            
     local   linear        = nn.Linear(lengWordVector, lengLabel)
@@ -34,8 +37,24 @@ function Seq2Seq:__init()
                                 :add(nn.Sequencer(self.lstmDecoder:maskZero(1)))
                                 :add(nn.Sequencer(nn.MaskZero(linear,1)))
                                 :add(nn.Sequencer(nn.MaskZero(logSofmax,1)))
-                
     --
+    -- init attention layer 
+    -- {input1, input2} => output
+    --      input1 =  out of encoder{1,SeqLengEncode}
+    --      input2 =  hidden state of decoder{t} 
+
+    local   extract = nn.ConcatTable()  :add (nn.Identity())    -- encode 
+                                        :add (nn.Identity())    -- decode
+    self.attention    = nn.ParallelTable()
+
+    --
+    -- init criterion 
+    if batchSize > 1 then
+        self.criterion = nn.SequencerCriterion(nn.MaskZeroCriterion(nn.ClassNLLCriterion(),1))
+    else
+        self.criterion = nn.SequencerCriterion(nn.ClassNLLCriterion())
+    end
+
     -- test model 
     -- x  = torch.Tensor({{0,2,5}, {3,4,5}}):t()
     -- print (x)
@@ -59,9 +78,18 @@ function Seq2Seq:backwardConnect(inputSeqLen)
 end
 
 --[[ Forward ]]--
-function Seq2Seq:forward(inputEncode, inputDecode)
-    self.encoder:forward(inputEncode)
+function Seq2Seq:forward(inputEncode, inputDecode, targetDecode)
+    self.outputEncode = self.encoder:forward(inputEncode)
     self:forwardConnect(inputEncode:size(1))
-    out = self.decoder:forward(inputDecode)
-    print(out)
+    self.outputDecode = self.decoder:forward(inputDecode)
+    local loss = self.criterion:forward(self.outputDecode, targetDecode)
+    print(loss)
+end
+
+--[[ Backward ]]--
+function Seq2Seq:backward(inputEncode, inputDecode, targetDecode)
+    local dloss_dx = self.criterion:backward(self.outputDecode, targetDecode)
+    self.decoder:backward(inputDecode, dloss_dx)
+    self:backwardConnect(inputEncode:size(1))
+    self.encoder:backward(inputEncode, self.outputEncode:zero())
 end
