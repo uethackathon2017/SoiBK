458f649dfe1b045b05fdd3c982b355ce33ba6b59
Nguyen Binh <nguyenvulebinh@gmail.com>
2017-03-10 23:51:58 +0700

    demo chat app

---
+++ frontend/ReactChat/app/src/main/java/soibk/hust/reactchat/model/Friend.java
@@ -0,0 +1,8 @@
+package soibk.hust.reactchat.model;
+
+
+
+public class Friend extends User {
+    public String id;
+    public String idRoom;
+}
---
+++ frontend/ReactChat/app/src/main/java/soibk/hust/reactchat/model/Message.java
@@ -0,0 +1,10 @@
+package soibk.hust.reactchat.model;
+
+
+
+public class Message {
+    public String idSender;
+    public String idReceiver;
+    public String text;
+    public long timestamp;
+}
